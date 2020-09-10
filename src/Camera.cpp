99cbc49894b13e479fbdeadb2f68c1f91b6bf505
Ian Shaw <shaw@pixel-sf.com>
2020-09-10 18:23:33 -0400

    bronze dragon

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -5,17 +5,25 @@ Camera::Camera(GLFWwindow* window) : window(window) {}
 void Camera::move() {
     float multiplier = 5.0f;
     if (glfwGetKey(window, GLFW_KEY_W))
-        position.y += 0.2f * multiplier;
+        position.z -= 0.2f * multiplier;
     if (glfwGetKey(window, GLFW_KEY_A))
         position.x -= 0.2f * multiplier;
     if (glfwGetKey(window, GLFW_KEY_S))
-        position.y -= 0.2f * multiplier;
+        position.z += 0.2f * multiplier;
     if (glfwGetKey(window, GLFW_KEY_D))
         position.x += 0.2f * multiplier;
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT))
+        position.y -= 0.2f * multiplier;
+    if (glfwGetKey(window, GLFW_KEY_SPACE))
+        position.y += 0.2f * multiplier;
+
+
+
     if (glfwGetKey(window, GLFW_KEY_Q))
-        position.z -= 0.2f * multiplier;
+        rotation.y -= 0.2f * multiplier;
     if (glfwGetKey(window, GLFW_KEY_E))
-        position.z += 0.2f * multiplier;
+        rotation.y += 0.2f * multiplier;
+
 }
 
 glm::vec3 Camera::getRotation() {
--- src/main.cpp
+++ src/main.cpp
@@ -28,17 +28,18 @@ int main(int argc, char** argv)
 
     Camera camera = Camera(display.getWindow());
 
+    // Cubes
     RawModel cube_raw = OBJLoader::loadObjModel("cube", loader);
     ModelTexture cube_texture = ModelTexture(loader.loadTexture("grass_block.png"));
-    cube_texture.setShineDamper(10);
+    cube_texture.setShineDamper(35);
     cube_texture.setReflectivity(0.2);
     TexturedModel cube_model = TexturedModel(cube_raw, cube_texture);
 
     std::vector<Entity> cubes;
-    for (int i=0; i<3000; i++) {
+    for (int i=0; i<1000; i++) {
         Entity cube = Entity(cube_model, 
                 glm::vec3(Random::integer(-100,100), 
-                          Random::integer(-100,100), 
+                          Random::integer(0,100), 
                           Random::integer(-200,200)),
                 glm::vec3(Random::integer(0,360),
                           Random::integer(0,360),
@@ -46,10 +47,22 @@ int main(int argc, char** argv)
         cubes.push_back(cube);
     }
 
+    // Dragons
+    RawModel dragon_raw = OBJLoader::loadObjModel("dragon", loader);
+    ModelTexture dragon_texture = ModelTexture(loader.loadTexture("bronze.png"));
+    dragon_texture.setShineDamper(10);
+    dragon_texture.setReflectivity(0.85);
+    TexturedModel dragon_model = TexturedModel(dragon_raw, dragon_texture);
+    Entity dragon = Entity(dragon_model, glm::vec3(-30, 0, -50), glm::vec3(0, 0, 0));
+
+
+    // Terrain
     ModelTexture terrain_texture = ModelTexture(loader.loadTexture("grass.png"));
-    ModelTexture terrain_texture2 = ModelTexture(loader.loadTexture("ground_grass_gen_10.png"));
-    Terrain terrain = Terrain(0, 0, loader, terrain_texture);
-    Terrain terrain2 = Terrain(1, 0, loader, terrain_texture2);
+    //ModelTexture terrain_texture2 = ModelTexture(loader.loadTexture("ground_grass_gen_10.png"));
+    Terrain terrain1 = Terrain(0, 0, loader, terrain_texture);
+    Terrain terrain2 = Terrain(0, 1, loader, terrain_texture);
+    Terrain terrain3 = Terrain(1, 0, loader, terrain_texture);
+    Terrain terrain4 = Terrain(1, 1, loader, terrain_texture);
 
     MasterRenderer renderer = MasterRenderer(display);
 
@@ -59,14 +72,19 @@ int main(int argc, char** argv)
 
         camera.move();
 
-        renderer.processTerrain(terrain);
+        renderer.processTerrain(terrain1);
         renderer.processTerrain(terrain2);
+        renderer.processTerrain(terrain3);
+        renderer.processTerrain(terrain4);
 
         //for (auto& cube: cubes) {
             //cube.rotate(1,2,3);
             //renderer.processEntity(cube);
         //}
-        
+
+        dragon.rotate(0,1,0);
+        renderer.processEntity(dragon);
+       
         renderer.render(light, camera);
 
         display.update();
